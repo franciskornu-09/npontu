a90d1f2658f3d140541b82bfa1b16f1a4231c3e9
fkornu <franciskornu@gmail.com>
2017-03-07 13:50:17 +0000

    update

--- contact.js
+++ contact.js
@@ -17,22 +17,23 @@
 // })();
 
 function contact(){
-	var name = $("#search").val();
+// 	var name = $("#search").val();
 	
-	function onSuccess(contacts) {
-    alert('Found ' + contacts.length + ' contacts.');
-};
+// 	function onSuccess(contacts) {
+//     alert('Found ' + contacts.length + ' contacts.');
+// };
 
-function onError(contactError) {
-    alert('onError!');
-};
+// function onError(contactError) {
+//     alert('onError!');
+// };
 
-// find all contacts with 'Bob' in any name field
-var options      = new ContactFindOptions();
-options.filter   = name;
-options.multiple = true;
-options.desiredFields = [navigator.contacts.fieldType.id];
-options.hasPhoneNumber = true;
-var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
-navigator.contacts.find(fields, onSuccess, onError, options);
+// // find all contacts with 'Bob' in any name field
+// var options      = new ContactFindOptions();
+// options.filter   = name;
+// options.multiple = true;
+// options.desiredFields = [navigator.contacts.fieldType.id];
+// options.hasPhoneNumber = true;
+// var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+// navigator.contacts.find(fields, onSuccess, onError, options);
+alert("this works");
 }
